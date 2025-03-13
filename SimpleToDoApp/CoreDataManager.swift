5d72e71e21a669cf81d910410dd6877613efa2e9
Асхат Баймуканов <askhatbaimukanov@Ashats-MacBook-Air.local>
2025-03-13 16:34:32 +0500

    create project with coredata, create model and entity

---
+++ SimpleToDoApp/CoreDataManager.swift
@@ -0,0 +1,79 @@
+//
+//  CoreDataManager.swift
+//  SimpleToDoApp
+//
+//  Created by Асхат Баймуканов on 11.03.2025.
+//
+
+import CoreData
+
+final class CoreDataManager {
+    static let shared = CoreDataManager() // Singleton
+
+    let persistentContainer: NSPersistentContainer
+
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "SimpleToDoApp") // Название должно совпадать с Core Data Model
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                fatalError("Ошибка загрузки Core Data: \(error)")
+            }
+        }
+    }
+
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+
+    // Сохранение контекста
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                fatalError("Ошибка сохранения Core Data: \(error)")
+            }
+        }
+    }
+    
+    // MARK: - CRUD Методы
+
+      // 1. Создание задачи
+      func createTask(title: String, description: String) {
+          let newTask = Task(context: context)
+          newTask.id = UUID()
+          newTask.title = title
+          newTask.taskDescription = description
+          newTask.createdAt = Date()
+          newTask.isCompleted = false
+
+          saveContext()
+      }
+
+      // 2. Получение всех задач
+      func fetchTasks() -> [Task] {
+          let request: NSFetchRequest<Task> = Task.fetchRequest()
+          request.sortDescriptors = [NSSortDescriptor(key: "createdAt", ascending: false)]
+
+          do {
+              return try context.fetch(request)
+          } catch {
+              print("Ошибка загрузки задач: \(error)")
+              return []
+          }
+      }
+
+      // 3. Обновление статуса задачи (выполнена/не выполнена)
+      func toggleTaskCompletion(task: Task) {
+          task.isCompleted.toggle()
+          saveContext()
+      }
+
+      // 4. Удаление задачи
+      func deleteTask(task: Task) {
+          context.delete(task)
+          saveContext()
+      }
+}
+
---
+++ SimpleToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleToDoApp
+//
+//  Created by Асхат Баймуканов on 11.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
